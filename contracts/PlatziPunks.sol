77fbfc0628a29e96ddccb648517306f1c1e3bfa2
Seba Itokazu <sebitokazu@gmail.com>
2021-11-15 20:43:06 -0300

    Counter impl

--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -3,8 +3,43 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract PlatziPunks is ERC721{
 
-    constructor() ERC721("PlatziPunks","PLPKS"){}
+contract PlatziPunks is ERC721, ERC721Enumerable{
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    uint256 public maxSupply;
+
+    constructor(uint256 _maxSupply) ERC721("PlatziPunks","PLPKS"){
+        maxSupply = _maxSupply;
+    }
+
+    function mint() public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        require (tokenId <= maxSupply, "All PlatziPunks are minted.");
+        _safeMint(msg.sender, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
 }
